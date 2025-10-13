53af2fab4286a06a790461461f1c5801bf3aa567
Rhodri McNamara <McNamaraRhodri@Gmail.com>
2025-10-13 11:33:42 -0400

    did stuff that professor did in class.(some of the exact numbers might be off)

--- Generic-Classes/src/main/java/io/github/OptimusRohan/Main.java
+++ Generic-Classes/src/main/java/io/github/OptimusRohan/Main.java
@@ -1,9 +1,43 @@
 package io.github.OptimusRohan;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Main {
+
+
+
+    public static int add(int a, int b) {
+        return a+b;
+    }
+    public static double add(double a, double b) {
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        Integer a = 5;
+        Integer b = 6;
+        Integer c = a+b;
+        System.out.println(c);
+        List<Integer> intArray = new ArrayList<>();
+        intArray.add(12);
+        intArray.add(76);
+
+        Integer boxedInt = 5;
+        Character boxedChar = 'a';
+        Boolean boxedBool = true;
+        Long boxedLong = 100L;
+        Double boxedDouble = 5.67;
+        Float boxedFloat = 43.2f;
+        Short boxedShort = 4;
+        Byte boxedByte = 2;
+
+        List<String> stringArray = new ArrayList<>();
+        stringArray.add("First");
+        stringArray.add("Second");
+
+        for (String str : stringArray) {
+            System.out.println(str);
+        }
     }
 }
