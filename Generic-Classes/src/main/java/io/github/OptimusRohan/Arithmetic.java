7526978f807619b3cf56ca782e694db6ddb6c702
Rhodri McNamara <McNamaraRhodri@Gmail.com>
2025-10-17 09:13:22 -0400

    This is what we did last class

---
+++ Generic-Classes/src/main/java/io/github/OptimusRohan/Arithmetic.java
@@ -0,0 +1,17 @@
+package io.github.OptimusRohan;
+
+/**
+ * Template class for arithmetic operations.
+ */
+public class Arithmetic <T extends Number>{
+    private T firstNumber;
+    private T secondNumber;
+
+
+    public Arithmetic(T firstNumber, T secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    public
+}
